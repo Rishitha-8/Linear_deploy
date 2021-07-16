160e73b99caad59edec1104030a4cb4441707b9b
Rishitha-8 <79561409+Rishitha-8@users.noreply.github.com>
2021-07-16 13:11:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,52 +4,42 @@ Created on Sun Jul 11 08:21:20 2021
 
 @author: NiruSai
 """
-
 import numpy as np
 import pickle
 import pandas as pd
 import streamlit as st 
 import os
 
-
 pickle_in=open("classifier.pkl","rb")
-model=pickle.load(pickle_in)
-
-
+classifier=pickle.load(pickle_in)
 
 def welcome():
     return "Welcome All"
 
 
-def predict_stock(Volume, Open, High, Low):
-    prediction=model.predict([[Volume, Open, High, Low]])
+def predict_note_authentication(Volume, Open, High, Low):
+    prediction=classifier.predict([[Volume, Open, High, Low]])
     print(prediction)
     return prediction
 
 
 def main():
-    st.title("STOCK PREDICTION USING LINEAR REGRESSION MODEL")
+    st.title("stock prediction")
     html_temp = """
-    <div style="background-color:lightpink;padding:10px">
-    <h2 style="color:black;text-align:center;">Stock Prediction App Using Streamlit </h2>
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit stock prediction ML App </h2>
     </div>
     """
-    
     st.markdown(html_temp,unsafe_allow_html=True)
-    Volume = st.text_input("Volume")
-    Open = st.text_input("Open")
-    High = st.text_input("High")
-    Low = st.text_input("Low")
+    Volume = st.text_input("volume","Type Here")
+    Open = st.text_input("open","Type Here")
+    High = st.text_input("high","Type Here")
+    Low = st.text_input("low","Type Here")
     result=""
     if st.button("Predict"):
-        result=predict_stock(Volume, Open, High, Low)
+        result=predict_note_authentication(Volume, Open, High, Low)
     st.success('The output is {}'.format(result))
-    display_code = st.radio("Would you like to display the code?", ("Yes", "No"))
-
-    if display_code == "Yes":
-        with st.echo():
-            
-            
-   if __name__=='__main__':
+    
+if __name__=='__main__':
     main()
 

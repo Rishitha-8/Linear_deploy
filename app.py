9fb78cce6870b23b537c82861a7e0efcf308432b
Rishitha-8 <79561409+Rishitha-8@users.noreply.github.com>
2021-07-16 13:32:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def main():
     st.success('The output is {}'.format(result))
     radio = st.radio(label="", options=["Yes", "No"])
     if radio == "Yes":
-        main=open("app.py","rb")
+        main=open("reqirements.txt","rb")
         st.write(main)
      
     

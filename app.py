822a92bf7a9dc53fb14e69639d69dc5fb0185448
Rishitha-8 <79561409+Rishitha-8@users.noreply.github.com>
2021-07-16 13:14:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def main():
     st.title("stock prediction")
     html_temp = """
     <div style="background-color:pink;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit stock prediction ML App </h2>
+    <h2 style="color:black;text-align:center;">Streamlit stock prediction ML App </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
